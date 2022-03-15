7dbe99a7166309817cfbe11d450337df4bd5561d
TSC <tsc@TSCs-iMac.local>
2022-03-15 15:36:41 +0700

    vue cli app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<h1>Whose click:{{clicker}}</h1>
+<ButtonCount :title="title" :user="users[0]" parent="child" v-on:handleClickCount="detectUserClick($event)"/>
+<ButtonCount :user="users[1]"  v-on:handleClickCount="detectUserClick($event)"/>
+<ButtonCount :user="users[2]" v-on:handleClickCount="detectUserClick($event)"/>
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import ButtonCount from './components/ButtonCount.vue'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {   
+    ButtonCount
+  },
+  data(){
+    return  {
+      title:'First Vue App',
+      users:['Borin','Tola', 'Dara'],
+      clicker:[]
+    }
+  },
+  methods:{
+    detectUserClick(value){
+      console.log(value)
+      // this.clicker.prototype.filter(item=>{
+      //   if(value!=item && length(this.clicker)>0){
+      //     this.clicker.push(item)
+      //   }
+      // })
+      this.clicker.push(value)
+      console.log("Who clicked:",this.clicker)
+    }
+
   }
 }
 </script>
---
+++ src/components/ButtonCount.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <button v-on:click="handleClickCount" > Clicker {{user}}: {{count}}</button>
+        <input type="text" v-bind:value="count">
+    </div>
+        <p>Count: {{count}}</p>
+    
+</template>
+<script>
+
+export default({
+    props:['title','user'],
+    data(){
+        return {
+            count: 0
+        }
+    },
+    methods:{
+        handleClickCount(){
+            console.log(this.count)
+            this.count++
+            console.log("props",this.user)
+            this.$emit('handleClickCount', this.user)
+           // this.countJoke()
+        },
+        countJoke(){
+                if(this.count==2){
+                    alert("Wow,Magic happened")
+                }
+        }
+    },
+
+})
+</script>
+<style scoped>
+
+</style>
